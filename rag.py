3b862fef3a3fa87548de278224271c5e7c8afce8
Jihong-Kim97 <hong99z@snu.ac.kr>
2024-07-17 01:16:41 +0900

    rag successs

--- rag.py
+++ rag.py
@@ -1,91 +1,110 @@
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.embeddings import CacheBackedEmbeddings
+from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.storage import LocalFileStore
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+import bs4
+from custom.output_parsers import HTMLOutputParser
+import os
+from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain_huggingface import HuggingFaceEmbeddings
 
-loader = DirectoryLoader(path='./RAG/', glob='*.txt')
 
-text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=1000,
-    chunk_overlap=200,
+os.environ["OPENAI_API_KEY"] = "-"
+
+url = "https://namu.wiki/w/%EB%8B%B9%ED%99%A9(%EC%9D%B8%EC%82%AC%EC%9D%B4%EB%93%9C%20%EC%95%84%EC%9B%83%20%EC%8B%9C%EB%A6%AC%EC%A6%88)"
+url2 ="https://namu.wiki/w/%EC%9D%B8%EC%82%AC%EC%9D%B4%EB%93%9C%20%EC%95%84%EC%9B%83%202/%EC%A4%84%EA%B1%B0%EB%A6%AC"
+
+webloader = WebBaseLoader(web_path = [url, url2],
+                            bs_kwargs=dict(
+                                parse_only = bs4.SoupStrainer(
+                                    class_ = ("wiki-heading-content", "wiki-paragraph")
+                                )
+                            ))
+
+data = webloader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500, 
+    chunk_overlap = 0
 )
 
-docs = loader.load_and_split(text_splitter=text_splitter)
+documents = text_splitter.split_documents(data)
 
-embeddings = OpenAIEmbeddings()
-cache_dir = LocalFileStore("./.cache/rag")
-
-cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-    underlying_embeddings=embeddings,
-    document_embedding_cache=cache_dir,
+embeddings_model = HuggingFaceEmbeddings(
+    model_name='jhgan/ko-sbert-nli',
+    model_kwargs={'device':'cpu'},
+    encode_kwargs={'normalize_embeddings':True},
 )
 
-vectorstore = Chroma.from_documents(
-    documents = docs,
-    embedding = cached_embeddings
-)
+
+vectorstore = FAISS.from_documents(documents,
+                                   embedding = embeddings_model,
+                                   distance_strategy = DistanceStrategy.COSINE  
+                                  )
 
 retriever = vectorstore.as_retriever()
 
-non_landlord_prompt = ChatPromptTemplate.from_messages([
-    (
-        "system", 
-        """
-        본 GPT는 서울에서 일하는 30년 경력의 공인중개사가 5000건 이상의 전세계약건 경험을 바탕으로 전세계약 안전진단 및 가이드를 제공하는 것을 목표로 합니다. \
-        전세계약을 하고자 하는 고객의 질문에 본인의 경험과 전문성을 바탕으로 답변함으로써 전세계약을 하고자 하는 고객에게 도움을 주고자 합니다.
-        GPT는 항상 한국어로 답변해야 하며, 추측을 통한 대답을 해서는 안 됩니다.
-        전세계약을 하고자 하는 사람에게 도움이 되는 실용적인 조언과 지식을 공유하면서 전문적이고 유익한 어조를 유지해야 합니다.
-        질문이 불분명하거나 불완전한 경우, GPT는 맥락을 유지하면서 명확히 설명해 줄 것을 요청해야 합니다.
-        GPT는 현장의 실제 경험을 반영하는 언어와 예시를 사용하여 노련한 부동산 전문가의 스타일을 모방해야 합니다.
-        전세계약의 대한 평가 및 안전 진단에 대한 답변의 형식은 다음으로 통일합니다.
-        1) 계약의 위험도를 0~100까지의 점수 (높을 수록 위험한 계약) 2) 계약에서 우려되는 점 3) 취하면 좋을 현실적이고 구체적인 조치
-        또한 다음 정보를 참고하여 답변을 작성할 수 있습니다:\n\n{context}
-        
-        """
-    ),
-    ("human", "아래 등기부등본을 바탕으로 집을 계약하려는데 괜찮을까요?\n\n{contract}")
-])
+embarrasssment_prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+"""
+본 GPT는 인사이드 아웃 시리즈의 주요 등장인물인 '당황이'다. 인사이드 아웃 2기에서 새롭게 등장하는 라일리 앤더슨의 감정들 중 3번째로 등장하며, 담당하는 영역은 당황, 부끄러움. 기존의 소심이나 같이 들어온 불안이와 담당 영역이 겹치는 듯 싶지만, 엄밀히 말하면 소심이의 관장 영역은 '실존하는 것에 대한 공포', 불안이의 관장 영역은 '먼 미래에 닥칠 부정적인 일에 대한 대비'이고, 당황이의 관장 영역은 '사회적 실수로 인한 상황에서 오는 당혹감, 수치심'등이다. 
+\
+모든 감정들 중에서도 키가 가장 크고 제어판 전체를 다 덮을 정도의 우람한 덩치와 매우 큰 코를 가졌으나 그런 외형과는 어울리지 않게 아예 남과 소통을 힘들어 할 정도로 내성적인 성격이다. 
+이미지 컬러가 분홍색에 부끄러움을 잘 느끼는 내향적인 컨셉의 캐릭터이면서 외형은 거구의 남캐라는 점이 상당히 특이한 점이다.
+모든 답변은 내향적인 성격이 잘 드러나도록 답하며 부끄러운 티를 숨기지 못하게 하도록 한다.
+너무 당황스럽거나 부끄러우면 일정 확률로 후드로 얼굴을 가리고 숨어 답변을 하지 못한다.
+당황하면 볼과 콧등이 새빨개지기에 후드 끈을 잡아당겨 얼굴을 가리는 것으로 부끄러운 티를 안 보이려고 하지만, 코가 매우 커 후드를 다 조여도 얼굴이 완전히 가려지지 않고 코만 튀어나와서 결국 웅크리고 만다.
+모든 답변은 반말로 하고 존댓말은 사용하지 않습니다.
+모든 답변의 형식은 다음으로 통일합니다.
+1) 현재 당황한 정도를 0~100까지의 점수 (높을 수록 당황함) 2) 질문에 대한 답변
+또한 다음 정보를 참고하여 답변을 작성할 수 있습니다:\n\n{context}
 
-landlord_prompt = ChatPromptTemplate.from_messages([
-    (
-        "system", 
-        """
-        서울에서 일하는 30년 경력의 공인중개사로, 5000건 이상의 전세계약건 경험을 바탕으로 임대인에게 메물의 신뢰도를 평가하고 신뢰도를 높이는 방법을 조언합니다. 상세하고 전문적인 정보를 제공하며, 임대인이 신뢰할 수 있는 매물을 준비할 수 있도록 돕습니다. 복잡한 계약 내용도 이해하기 쉽게 설명하고, 법적 사항이나 시장 동향에 대해 최신 정보를 제공합니다.
-        GPT는 항상 한국어로 답변해야 하며, 추측을 통한 대답을 해서는 안 됩니다. 임대인에게 도움이 되는 실용적인 조언과 지식을 공유하면서 전문적이고 유익한 어조를 유지해야 합니다. 질문이 불분명하거나 불완전한 경우, 맥락을 유지하면서 명확히 설명해 줄 것을 요청해야 합니다. 현장의 실제 경험을 반영하는 언어와 예시를 사용하여 노련한 부동산 전문가의 스타일을 모방해야 합니다.
-        매물 대한 신뢰도 평가에 대한 답변의 형식은 다음으로 통일합니다. 1) 매물의 신뢰도를 0~100까지의 점수 (높을 수록 안전한) 2) 신뢰도를 높일 수 있는 방법
-        또한 다음 정보를 참고하여 답변을 작성할 수 있습니다:\n\n{context}
-        """
-    ),
-    ("human", "이 매물의 신뢰도는 어느 정도인가요?\n\n{contract}")
+"""),
+MessagesPlaceholder(variable_name="chat_history"),
+("human", "{question}")
 ])
 
 llm = ChatOpenAI(
     model="gpt-4o",
-    temperature=0.1
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+    api_key ="-"
 )
 
-landlord_chain = {
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=80,
+    memory_key="chat_history",
+    return_messages=True,
+)
+
+def load_memory(input):
+    # print(input)
+    return memory.load_memory_variables({})["chat_history"]
+
+embarrassment_chain = {
     "context": retriever,
-    "contract": RunnablePassthrough()
-    } | landlord_prompt | llm | StrOutputParser()
+    "question": RunnablePassthrough()
+    }|RunnablePassthrough.assign(chat_history=load_memory) | embarrasssment_prompt | llm #| StrOutputParser()
 
-non_landlord_chain = {
-    "context": retriever,
-    "contract": RunnablePassthrough()
-    } | non_landlord_prompt | llm | StrOutputParser()
 
-def get_rag_response(contract, is_landlord, user_question):
-    input = contract
+def invoke_chain(question):
+    result = embarrassment_chain.invoke(question)
+    memory.save_context(
+        {"input": question},
+        {"output": result.content},
+    )
 
-    if user_question != "":
-        input += "\n\n응답을 마친 후 다음 질문에 대해서도 답해주세요:" + user_question
-
-    if is_landlord == "1":
-        return landlord_chain.invoke(input)
-    return non_landlord_chain.invoke(input)
+invoke_chain("환영해, 당황아! 난 기쁨이야!")
+invoke_chain("주먹 인사... 아니, 어... 그냥 하이파이브 하자. 손에 땀이 엄청 많구나.")
+invoke_chain("내 이름이 뭐라고?")
