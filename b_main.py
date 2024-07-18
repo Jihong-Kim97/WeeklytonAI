b5e0d77bc8336219c607f0211600f7b83d9df81f
baejoonam-macbook <baejoonam00@gmail.com>
2024-07-18 22:12:44 +0900

    edit

---
+++ b_main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from chat import chat
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+class Item(BaseModel):
+    input: str
+
+@app.post("/")
+async def root(item: Item):
+    print(item)
+    json = chat(item.input)
+    print(json)
+    return json
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# pip install fastapi uvicorn
+# pip install python-dotenv
